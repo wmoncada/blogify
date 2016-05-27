1677e72c48499395897d70dc67dc3094c3f1b9ec
Walberto Moncada <monposmetal@gmail.com>
2016-05-26 23:03:34 -0600

    Tags entity added

---
+++ src/AppBundle/Entity/Tags.php
@@ -0,0 +1,84 @@
+<?php 
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tags")
+ */
+class Tags
+{
+	/**
+	 * @ORM\Column(type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+	/**
+	 * @ORM\Column(type="string", length=50)
+	 */
+	private $name;
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	private $frequency;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Tags
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set frequency
+     *
+     * @param integer $frequency
+     *
+     * @return Tags
+     */
+    public function setFrequency($frequency)
+    {
+        $this->frequency = $frequency;
+
+        return $this;
+    }
+
+    /**
+     * Get frequency
+     *
+     * @return integer
+     */
+    public function getFrequency()
+    {
+        return $this->frequency;
+    }
+}
