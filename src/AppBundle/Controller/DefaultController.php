6f3364604beb8063c5ad764ec88dbb7879fa7bdc
Walberto Moncada <monposmetal@gmail.com>
2016-05-30 16:09:21 -0600

    Obteniendo datos de la pagina principal

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,10 +13,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Posts');
+        $posts = $repo->findAll();
+
+        return $this->render('default/index.html.twig',array(
+                "posts" => $posts 
+            ));
     }
 
     /**
