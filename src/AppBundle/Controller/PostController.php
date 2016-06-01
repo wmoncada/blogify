5707630df00dc712e8e95950a8fa16205076ac07
Walberto Moncada <monposmetal@gmail.com>
2016-06-01 16:04:22 -0600

    Creada la clase para manejo de los post

---
+++ src/AppBundle/Controller/PostController.php
@@ -0,0 +1,17 @@
+<?php 
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PostController extends Controller
+{
+	/**
+	 * @Route("/post/create",name="post_create")
+	 */
+	 public function createAction()
+	 {
+
+	 } 
+}
