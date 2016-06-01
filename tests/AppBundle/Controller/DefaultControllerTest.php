af468b1d3bdff96138324ed739767eb1e6ff5238
Walberto Moncada <monposmetal@gmail.com>
2016-06-01 16:03:55 -0600

    Agregado test de url del articulo

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+	/**
+	 * 			TEST FUNCIONALES
+	 * 
+	 * Prueba la carga de la pagina principal del blog
+	 */
     public function testIndex()
     {
         $client = static::createClient();
@@ -14,5 +19,27 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Blogify', $crawler->filter('')->text());
+    }
+
+    /**
+     * Prueba la carga del articulo por urls invalidas 
+     */ 
+    public function testArticuloBadURL()
+    {
+    	$client =static::createClient();
+
+    	$crawler = $client->request('GET','/articulo/1');
+
+    	$this->assertNotEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Blogify', $crawler->filter('')->text());	
+    }
+
+    public function testArticuloURL()
+    {
+    	$client = static::createClient();
+
+    	$crawler = $client->request('GET','/articulo/1/asd');
+
+    	$this->assertEquals(200,$client->getResponse()->getStatusCode());
     }
 }
